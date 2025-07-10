2b37bd97d1b64b5541933e8f8f400da219efa409
Ian Pires de Campos <ianp259@gmail.com>
2025-07-10 17:37:11 -0300

    First version

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+#+------------------ import section ------------------+
 import discord
 from discord.ext import commands
 import logging
 from dotenv import load_dotenv
 import os
+import utils
 
+#+------------------ bot booting section ------------------+
 load_dotenv()
 
 token = os.getenv("DISCORD_TOKEN")
@@ -15,15 +18,51 @@ intents.members = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+#+------------------ on events ------------------+
 @bot.event
 async def on_ready():
     print(f"Helo, {bot.user.name}")
 
 @bot.event
 async def on_command_error(ctx, error):
+    """Handles wrong user input as commands"""
     if isinstance(error, commands.CommandNotFound):
         await ctx.send("Command not found. Try using !h for help")
     else:
         raise error
+
+#+------------------ commands ------------------+
+@bot.command()
+async def h(ctx):
+    await ctx.send(utils.help_text)
+
+@bot.command()
+async def add(ctx, *, task:str):
+    utils.add_task(task=task)
+    await ctx.send(f"Added **{task}** to your list")
+
+@bot.command()
+async def undone(ctx):
+    await ctx.send(utils.get_undone_tasks())
+
+@bot.command()
+async def finish(ctx, *, task_id):
+    utils.finish_task(utils.get_real_id(task_id))
+    await ctx.send(f"Finished task!")
+
+@bot.command()
+async def done(ctx):
+    await ctx.send(utils.get_done_tasks())
+
+@bot.command()
+async def list(ctx):
+    await ctx.send(utils.get_all_tasks())
+
+@bot.command()
+async def about(ctx):
+    ctx.send(utils.about)
+
+
+#+------------------ run bot ------------------+
 
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
--- utils.py
+++ utils.py
@@ -16,22 +16,82 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS tasks (
 def add_task(task: str) -> None:
     """Insert a task into tasks table"""
     cursor.execute(f"""INSERT INTO tasks (task)
-                   VALUES ('{task}')""")
+                   VALUES (?)""", (task,))
+    conn.commit()
 
 def finish_task(id:int) -> None:
     """Changes the status of the task with id"""
     cursor.execute(f"""UPDATE tasks
                    SET done = 1
-                   WHERE id = {id};""")
+                   WHERE id = (?);""", (id,))
+    conn.commit
 
 def get_undone_tasks() -> list[tuple[int, str, bool]]:
-    """returns all undone tasks"""
+    """returns a string of all undone tasks"""
+    undone_str = ""
     tasks = cursor.execute("""SELECT *
                            FROM tasks
-                           WHERE done = 0;""").fetchall()
-    return tasks
+                           WHERE done = 0
+                           ORDER BY id;""").fetchall()
+    for i in enumerate(tasks, start=1):
+        undone_str += f"{i[0]} - {i[1][1]}\n"
+    return undone_str
 
+def get_real_id(cur_id):
+    undone_tasks = cursor.execute("""SELECT *
+                                  FROM tasks
+                                  WHERE done = 0
+                                  ORDER BY id;""").fetchall()
+    real_id = undone_tasks[int(cur_id)-1][0]
+    return real_id
 
-    
+def get_done_tasks():
+    """returns a string of all done tasks"""
+    done_str = ""
+    tasks = cursor.execute("""SELECT *
+                           FROM tasks
+                           WHERE done = 1
+                           ORDER BY id;""").fetchall()
+    for i in enumerate(tasks, start=1):
+        done_str += f"{i[0]} - {i[1][1]}\n"
+    return done_str
 
+def get_all_tasks():
+    """returns a string of all tasks"""
+    tasks_str = ""
+    tasks = cursor.execute("""SELECT *
+                           FROM tasks
+                           ORDER BY id;""").fetchall()
+    for i in tasks:
+        if i[2]:
+            tasks_str += f"{i[0]} - {i[1]} 🟢\n"
+        else:
+            tasks_str += f"{i[0]} - {i[1]} 🔴\n"
+    return tasks_str
 
+#+---------------------- information ----------------------+
+help_text = """
+**Commands:**
+📝 **!add <task>**
+- Adds a task to your list
+
+📋 **!undone**
+- Lists all undone tasks
+
+📋 **!done**
+- Lists all done tasks
+
+📋 **!list**
+- Lists all tasks
+
+✅ **!finish <task_id>**
+- marks the task as done
+
+❓ **!about**
+- Information about the bot
+
+🆘 **!h**
+- Help message
+"""
+
+about = """🤖A task list bot for your server"""
